73a097dced254a7bfa3b6e524b0078896e4869cf
jee14k <jeevan.koshy@science.christuniversity.in>
2019-07-12 19:15:19 +0530

    Fest commit

--- app/src/main/java/com/example/fest/Confirm.java
+++ app/src/main/java/com/example/fest/Confirm.java
@@ -17,12 +17,16 @@ public class Confirm extends AppCompatActivity {
     String emailval;
     String collval;
     Button btnBack;
+    Button btex;
+    Button dob;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirm);
         btnBack = (Button) findViewById(R.id.button2);
+        btex = (Button) findViewById(R.id.button3);
+        dob = (Button) findViewById(R.id.button4);
         nametxt = (TextView) findViewById(R.id.textView11);
         mobiletxt = (TextView) findViewById(R.id.textView10);
         emailtxt = (TextView) findViewById(R.id.textView12);
@@ -35,6 +39,12 @@ public class Confirm extends AppCompatActivity {
         emailtxt.setText(emailval);
         collval = getIntent().getExtras().getString("Value4");
         colltext.setText(collval);
+        btex.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     public void btnRet(View v){
@@ -42,5 +52,13 @@ public class Confirm extends AppCompatActivity {
         startActivity(i);
         finish();
     }
+
+    public void btndob(View v){
+        Intent i = new Intent(this,DobAct.class);
+        startActivity(i);
+        finish();
+    }
+
+
 
 }
